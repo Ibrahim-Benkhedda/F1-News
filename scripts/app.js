486a5705b2259c5f26f0da0080bf199cb044afb8
Ibrahim-Benkhedda <63601488+Ibrahim-Benkhedda@users.noreply.github.com>
2022-08-31 23:33:38 +0100

    1.3
    ranking page

--- scripts/app.js
+++ scripts/app.js
@@ -4,8 +4,8 @@ let etape = 0;
 
 let nbr__img = img__slider.length;
 
-let precedent = document.querySelector('.precedent');
-let suivant = document.querySelector('.suivant');
+// let precedent = document.querySelector('.precedent');
+// let suivant = document.querySelector('.suivant');
 
 function enleverActiveImages() {
     for(let i = 0 ; i < nbr__img ; i++) {
@@ -13,23 +13,23 @@ function enleverActiveImages() {
     }
 }
 
-suivant.addEventListener('click', function() {
-    etape++;
-    if(etape >= nbr__img) {
-        etape = 0;
-    }
-    enleverActiveImages();
-    img__slider[etape].classList.add('active');
-})
-
-precedent.addEventListener('click', function() {
-    etape--;
-    if(etape < 0) {
-        etape = nbr__img - 1;
-    }
-    enleverActiveImages();
-    img__slider[etape].classList.add('active');
-})
+// suivant.addEventListener('click', function() {
+//     etape++;
+//     if(etape >= nbr__img) {
+//         etape = 0;
+//     }
+//     enleverActiveImages();
+//     img__slider[etape].classList.add('active');
+// })
+//
+// precedent.addEventListener('click', function() {
+//     etape--;
+//     if(etape < 0) {
+//         etape = nbr__img - 1;
+//     }
+//     enleverActiveImages();
+//     img__slider[etape].classList.add('active');
+// })
 
 setInterval(function() {
     etape++;
