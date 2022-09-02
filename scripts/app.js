039174f1717ab58013610695a8ba4fc5201fd36e
Ibrahim-Benkhedda <63601488+Ibrahim-Benkhedda@users.noreply.github.com>
2022-09-02 16:52:44 +0100

    1.05
    fixed about us page
    made it responsive
    added content for FAQ, timeline cards

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,4 @@
+// slider
 let img__slider = document.getElementsByClassName('img__slider');
 
 let etape = 0;
@@ -19,3 +20,20 @@ setInterval(function() {
     enleverActiveImages();
     img__slider[etape].classList.add('active');
 }, 3000)
+
+
+// FAQ cards
+const faqCols = document.querySelectorAll('.col-faq');
+ faqCols.forEach(function(faqCol) {
+   faqCol.addEventListener('click', function(e) {
+     // console.log('working');
+     currentCol = e.currentTarget.querySelector('p');
+
+     if (currentCol.classList.contains('active')) {
+       currentCol.classList.remove("active");
+     }
+     else {
+       currentCol.classList.add("active");
+     }
+   })
+ });
