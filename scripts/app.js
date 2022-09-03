fe6777c028006c923df5c3519f000caade6b21bd
Ibrahim-Benkhedda <63601488+Ibrahim-Benkhedda@users.noreply.github.com>
2022-09-03 20:17:49 +0100

    1.09
    dates and accessibility remains

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,42 @@
+// nav bar
+// based on https://www.youtube.com/watch?v=gXkqy0b4M5g
+
+// when the mouse is clicked on the burger
+// set class to active and translate it to the left
+function slideNav() {
+  // get the burger and the nav bar links and store them in variables
+  const burger = document.querySelector('.burger');
+  const nav = document.querySelector('.nav-links');
+  // gets all nav links
+  const navLinks = document.querySelectorAll('.nav-links a');
+
+
+  burger.addEventListener('click', function(){
+    // toggle nav bar
+    nav.classList.toggle('nav-active');
+
+    // animates the links of the nav bar with the index of the list
+    navLinks.forEach(function(link, index){
+      if (link.style.animation) {
+        // set it to null
+        ''
+      }
+      else {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 10}s`
+      }
+    })
+  })
+
+
+
+}
+
+// calls the function
+slideNav();
+
+
+
+
 // slider
 let img__slider = document.getElementsByClassName('img__slider');
 
